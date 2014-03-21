7f8c795f6a7c9a99aee3642251f2442240ff2c01
rahular <rahulife007@gmail.com>
2014-03-21 13:45:49 +0530

    replaced menu code

--- ZxingExample/src/com/zxing/example/ScannerActivity.java
+++ ZxingExample/src/com/zxing/example/ScannerActivity.java
@@ -3,16 +3,22 @@ package com.zxing.example;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.google.zxing.Result;
 import com.google.zxing.client.android.CaptureActivity;
 
 public class ScannerActivity extends CaptureActivity {
-	/** Called when the activity is first created. */
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_scanner);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		return false;
 	}
 
 	@Override
